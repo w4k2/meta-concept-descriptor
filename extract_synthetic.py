a768863bf0a5c88019984851a7023f097b739e2e
Joana <asiakom24@vp.pl>
2022-11-22 13:36:10 +0100

    first commit

---
+++ extract_synthetic.py
@@ -0,0 +1,127 @@
+import numpy as np
+import strlearn as sl
+from pymfe.mfe import MFE
+import matplotlib.pyplot as plt
+from utils import find_real_drift
+import time
+from tqdm import tqdm
+
+replications = 10
+random_states = np.random.randint(100,10000,replications)
+
+drift_types = {
+    'sudden': {'concept_sigmoid_spacing':9999, 'n_drifts': 20,},
+    'gradual': {'concept_sigmoid_spacing':5, 'n_drifts': 6},
+    'incremental': {'concept_sigmoid_spacing':5, 'incremental':True, 'n_drifts': 6}
+}
+
+
+stream_static = {
+                'n_chunks': 5000,
+                'chunk_size': 200,
+                'n_features': 10,
+                'n_informative': 10,
+                'n_redundant': 0,
+                'class_sep':1,
+            }
+
+measures = ["clustering",
+            "complexity",
+            "concept",
+            "general",
+            "info-theory",
+            "itemset",
+            "landmarking",
+            "model-based",
+            "statistical"
+            ]
+measures_len = [
+    8,
+    22,
+    4, 
+    11,
+    8,
+    2,
+    7,
+    14,
+    28
+]
+
+pbar = tqdm(total=len(drift_types) * len(random_states))
+
+for m_id, measure_key in enumerate(measures):
+    
+    out = np.zeros((len(drift_types), len(random_states), stream_static['n_chunks'], measures_len[m_id]+1))
+    pbar.reset()
+    print(measure_key)
+    for dt_id, dt in enumerate(drift_types):
+        for rep, rs in enumerate(random_states):
+
+            config = {
+                **stream_static,
+                **drift_types[dt],
+                'random_state': rs
+            }
+            
+            drifts = find_real_drift(config['n_chunks'], config['n_drifts'])
+            
+            stream = sl.streams.StreamGenerator(**config)
+            e = stream._sigmoid(
+                stream.concept_sigmoid_spacing, stream.n_drifts
+            )[1][::stream_static['chunk_size']]
+                        
+            concept=0
+            decreasing = True
+
+            for chunk in range(stream_static['n_chunks']): 
+                
+                #IDENTIFY CONCEPT
+                if dt=='sudden':
+                    if chunk in drifts:
+                        concept+=1 #chunk 125 (w drift) to juz nowa koncepcja
+                else:
+                    if decreasing:
+                        if concept%4==0:
+                            if e[chunk]<0.9:
+                                concept+=1
+                        if concept%4==1:
+                            if e[chunk]<0.75:
+                                concept+=1
+                        if concept%4==2:
+                            if e[chunk]<0.25:
+                                concept+=1
+                        if concept%4==3:
+                            if e[chunk]<0.1:
+                                concept+=1
+                                decreasing = False
+                    else:
+                        if concept%4==0:
+                            #szukamy punktu 0.1
+                            if e[chunk]>0.1:
+                                concept+=1
+                        if concept%4==1:
+                            #szukamy punktu 0.25
+                            if e[chunk]>0.25:
+                                concept+=1
+                        if concept%4==2:
+                            #szukamy punktu 0.75
+                            if e[chunk]>0.75:
+                                concept+=1
+                        if concept%4==3:
+                            #szukamy punktu 0.9
+                            if e[chunk]>0.9:
+                                concept+=1
+                                decreasing = True
+
+
+                # CALCULATE
+                X, y = stream.get_chunk()
+                
+                mfe = MFE(groups=[measure_key])
+                mfe.fit(X,y)
+                ft = mfe.extract()[1]
+                
+                out[dt_id, rep, chunk, :-1] = ft
+                out[dt_id, rep, chunk, -1] = concept
+                
+    np.save('res/%s.npy' % measure_key, out)
---
+++ utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def find_real_drift(chunks, drifts):
+    interval = round(chunks/drifts)
+    idx = [interval*(i+.5) for i in range(drifts)]
+    return np.array(idx).astype(int)
