13d713951ac3a457b02f61601ce84a095f17cbbc
Joana <asiakom24@vp.pl>
2022-11-29 18:23:03 +0100

    real

---
+++ analyze_real.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1233)
+
+measures = ["clustering",
+    "complexity",
+    "concept",
+    "general",
+    "info-theory",
+    "itemset",
+    "landmarking",
+    "model-based",
+    "statistical"
+    ]
+
+real_streams = [
+    'covtypeNorm-1-2vsAll',
+    'electricity',
+    'poker-lsn-1-2vsAll',
+    'INSECTS-abrupt',
+    'INSECTS-gradual',
+    'INSECTS-incremental'
+    ]
+
+base_clfs = ['GNB', 'KNN', 'SVM', 'DT', 'MLP']
+
+n_drift_types=3
+stream_reps=5
+
+res = np.load('res_clf_cls/real_clf.npy') # measures, datasets, reps, folds, clfs
+
+res_mean = np.mean(res, axis=2)
+print(res_mean.shape)
+
+fig, ax = plt.subplots(2, 3, figsize=(13,11), sharex=True, sharey=True)
+ax=ax.ravel()
+plt.suptitle('Real-world', fontsize=18)
+
+for dataset_id, dataset in enumerate(real_streams):
+        
+    axx = ax[dataset_id]
+    
+    r = res_mean[:,dataset_id]
+    axx.imshow(r, vmin=0.05, vmax=1.)
+    
+    for _a, __a in enumerate(measures):
+        for _b, __b in enumerate(base_clfs):
+            axx.text(_b, _a, "%.3f" % (r[_a, _b]) , va='center', ha='center', c='white', fontsize=11)
+
+    axx.set_title(dataset)
+    axx.set_xticks(np.arange(len(base_clfs)),base_clfs)
+    axx.set_yticks(np.arange(len(measures)),measures)
+    
+plt.tight_layout()
+plt.savefig('foo.png')
---
+++ clf_real.py
@@ -0,0 +1,78 @@
+import numpy as np
+from sklearn import clone
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+
+np.random.seed(1233)
+
+measures = ["clustering",
+        "complexity",
+        "concept",
+        "general",
+        "info-theory",
+        "itemset",
+        "landmarking",
+        "model-based",
+        "statistical"
+        ]
+
+real_streams_full = [
+    'real_streams/covtypeNorm-1-2vsAll-pruned.arff',
+    'real_streams/electricity.npy',
+    'real_streams/poker-lsn-1-2vsAll-pruned.arff',
+    'real_streams/INSECTS-abrupt_imbalanced_norm.arff',
+    'real_streams/INSECTS-gradual_imbalanced_norm.arff',
+    'real_streams/INSECTS-incremental_imbalanced_norm.arff'
+    ]
+
+base_clfs = [
+    GaussianNB(),
+    KNeighborsClassifier(),
+    SVC(random_state=11313),
+    DecisionTreeClassifier(random_state=11313),
+    MLPClassifier(random_state=11313)
+]
+
+origial_datasets=len(real_streams_full)
+
+n_splits=2
+n_repeats=5
+
+clf_res = np.zeros((len(measures), origial_datasets, n_splits*n_repeats, len(base_clfs)))
+pbar = tqdm(total=len(measures)*origial_datasets*n_splits*n_repeats*len(base_clfs))
+
+for f_id in range(len(real_streams_full)):
+    for m_id, m in enumerate(measures):
+        res = np.load('res/real_%s_%s.npy' % (f_id, m))
+        
+        p = np.random.permutation(res.shape[0])
+        res = res[p]
+    
+        # print(res_rep.shape) # chunks, measures + label
+        X = res[:,:-1]
+        y = res[:,-1]
+        
+        X[np.isnan(X)]=1
+        X[np.isinf(X)]=1
+        
+        rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=3242)
+        for fold, (train, test) in enumerate(rskf.split(X, y)):
+        
+            for base_id, base_c in enumerate(base_clfs):
+                clf = clone(base_c)
+                
+                pred = clf.fit(X[train], y[train]).predict(X[test])
+                acc = accuracy_score(y[test], pred)
+                
+                clf_res[m_id, f_id, fold, base_id] = acc
+                pbar.update(1)
+            
+        print(m, np.mean(clf_res[m_id, f_id], axis=0))
+    
+np.save('res_clf_cls/real_clf.npy', clf_res)
