32c741a1e88f623ee0662c4354ac3b2df400a08f
Joana <asiakom24@vp.pl>
2022-12-12 16:27:04 +0100

    .

--- analyze_semi.py
+++ analyze_semi.py
@@ -30,32 +30,28 @@ stream_reps=5
 res = np.load('res_clf_cls/semi_clf.npy') # measures, datasets, drifts, reps, folds, clfs
 res = res[:,:,:2]
 
-res_mean = np.mean(res, axis=(3))
-print(res_mean.shape)
+res_mean = np.mean(res, axis=(2,3))
 
-fig, ax = plt.subplots(2, 6, figsize=(20,10), sharex=True, sharey=True)
+fig, ax = plt.subplots(2, 3, figsize=(10,10), sharex=True, sharey=True)
+ax = ax.ravel()
 plt.suptitle('Semi-synthetic', fontsize=18)
 
 for dataset_id, dataset in enumerate(static_data):
-    for drift_type_id, drift_type in enumerate(['Nearest', 'Cubic']):
         
-        axx = ax[drift_type_id, dataset_id]
-        
-        r = res_mean[:,dataset_id,drift_type_id]
-        axx.imshow(r, vmin=0.05, vmax=1., cmap='Blues')
-        
-        for _a, __a in enumerate(measures):
-            for _b, __b in enumerate(base_clfs):
-                axx.text(_b, _a, "%.3f" % (r[_a, _b]) , va='center', ha='center', c='black' if r[_a, _b]<0.5 else 'white', fontsize=11)
-        
-        if drift_type_id==0:
-            axx.set_title(dataset)
-        else:
-            axx.set_xticks(np.arange(len(base_clfs)),base_clfs)
+    axx = ax[dataset_id]
+    
+    r = res_mean[:,dataset_id]
+    axx.imshow(r, vmin=0.05, vmax=1., cmap='Blues')
+    
+    for _a, __a in enumerate(measures):
+        for _b, __b in enumerate(base_clfs):
+            axx.text(_b, _a, "%.3f" % (r[_a, _b]) , va='center', ha='center', c='black' if r[_a, _b]<0.5 else 'white', fontsize=11)
+    
+    axx.set_title(dataset)
+    axx.set_xticks(np.arange(len(base_clfs)),base_clfs)
 
-        if dataset_id==0:
-            axx.set_ylabel(drift_type)
-            axx.set_yticks(np.arange(len(measures)),measures)
+    if dataset_id==0:
+        axx.set_yticks(np.arange(len(measures)),measures)
         
         
 plt.tight_layout()
